a5e9ae86f4582469a37b7ff53ae79e4b89a199d7
mzk88 <61551924+mzk88@users.noreply.github.com>
2020-03-03 23:24:03 +0900

    initial commit

---
+++ mzk/js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  })
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 200){
+      $(this).addClass('scrollin');
+      }
+    });
+  });
+});
